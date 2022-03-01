a616f8b344a9af63518c20c923c36a3a2a8fc858
lwmfjc <lwmfjc@gmail.com>
2022-03-01 15:12:15 +0800

    收发邮件

--- src/main/java/com/ztx/EmailUtils.java
+++ src/main/java/com/ztx/EmailUtils.java
@@ -2,9 +2,7 @@ package com.ztx;
 
 import com.sun.mail.imap.IMAPStore;
 
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
@@ -22,16 +20,85 @@ public class EmailUtils {
 
     private static String dirPath = System.getProperty("user.dir");
 
-    public static void getEmails() throws MessagingException, IOException {
+    public static void getEmailsByPop3() throws MessagingException, IOException {
         // 定义连接POP3服务器的属性信息
         String popServer = "pop.163.com";
         String popProtocol = "pop3";
 
-        String imapServer = "imap.163.com";
+        String username = "lwm_fjc@163.com";
+        String password = "AUZNEWSMRKJRNPNV"; // 163邮箱的SMTP的授权码，什么是授权码，它又是如何设置？
+
+        //String username = "811011902@qq.com";
+        //String password = "asdfhxujstlcbfjg"; // 163邮箱的SMTP的授权码，什么是授权码，它又是如何设置？
+
+        Properties props = new Properties();
+        //props.setProperty("mail.transport.protocol", popProtocol); // 使用的协议（JavaMail规范要求）
+        props.setProperty("mail.transport.protocol", popProtocol); // 使用的协议（JavaMail规范要求）
+        props.setProperty("mail.smtp.host", popServer); // 发件人的邮箱的 SMTP服务器地址
+
+        // 获取连接
+        Session session = Session.getDefaultInstance(props);
+        session.setDebug(false);
+
+        Store store = session.getStore(popProtocol);
+        store.connect(popServer, username, password); // POP3服务器的登陆认证
+
+        // 通过POP3协议获得Store对象调用这个方法时，邮件夹名称只能指定为"INBOX"
+        Folder folder = store.getFolder("INBOX");// 获得用户的邮件帐户
+        folder.open(Folder.READ_WRITE); // 设置对邮件帐户的访问权限
+
+        /*Message[] messages = folder.search(new SearchTerm() {
+            @Override
+            public boolean match(Message message) {
+                boolean isMatch = true;
+                try {
+                    isMatch = message.getFlags().contains(Flags.Flag.SEEN);
+                } catch (MessagingException e) {
+                    e.printStackTrace();
+                }
+                return  isMatch;
+
+            }
+        });*/
+        Message[] messages = folder.getMessages();// 得到邮箱帐户中的所有邮件
+
+        for (Message msg : messages) {
+            String subject = msg.getSubject();// 获得邮件主题
+
+            Address from = (Address) msg.getFrom()[0];// 获得发送者地址
+            System.out.println("邮件的主题为: " + subject + "\t发件人地址为: " + from
+                    + "邮件发送时间:" + msg.getSentDate().getTime());
+            if (msg.getReceivedDate() != null) {
+                System.out.println("接收时间" + msg.getReceivedDate());
+            }
+
+
+            // 邮件类型不是mixed时，表示邮件中不包含附件，直接输出邮件内容
+            if (!msg.isMimeType("multipart/mixed")) {
+
+            } else {
+
+            }
+        }
+
+        folder.close(false);// 关闭邮件夹对象
+        store.close(); // 关闭连接对象
+    }
+
+    public static void getEmailsByImap() throws MessagingException, IOException {
+        // 定义连接POP3服务器的属性信息
+        String popServer = "pop.163.com";
+        String popProtocol = "pop3";
+
         String protocolImap = "imap";
 
-        String username = "lwm_fjc@163.com";
-        String password = "AUZNEWSMRKJRNPNV"; // QQ邮箱的SMTP的授权码，什么是授权码，它又是如何设置？
+        //String imapServer = "imap.163.com";
+        // String username = "lwm_fjc@163.com";
+        //String password = "AUZNEWSMRKJRNPNV"; // QQ邮箱的SMTP的授权码，什么是授权码，它又是如何设置？
+
+        String imapServer = "imap.qq.com";
+        String username = "811011902@qq.com";
+        String password = "asdfhxujstlcbfjg"; // 163邮箱的SMTP的授权码，什么是授权码，它又是如何设置？
 
         Properties props = new Properties();
         //props.setProperty("mail.transport.protocol", protocolImap); // 使用的协议（JavaMail规范要求）
@@ -71,7 +138,7 @@ public class EmailUtils {
                 } catch (MessagingException e) {
                     e.printStackTrace();
                 }
-                return  isMatch;
+                return !isMatch;
 
             }
         });
--- src/main/java/com/ztx/Main.java
+++ src/main/java/com/ztx/Main.java
@@ -1,16 +1,11 @@
 package com.ztx;
 
-import javax.mail.MessagingException;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 public class Main {
     public static void main(String[] args) throws Exception {
         //System.out.println();
         //EmailUtils.sendEmail();
-        //EmailUtils.getEmails();
-        // EmailUtils.sendEmail();
-        EmailUtils.getEmails();
+        EmailUtils.getEmailsByPop3();
+        //EmailUtils.sendEmail();
+        //EmailUtils.getEmailsByPop3();
     }
 }
